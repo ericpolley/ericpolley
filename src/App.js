e63116af8ef60076b4af77345a88c27c8af546fe
eric polley <ericpolley@live.com>
2022-12-14 14:07:19 -0800

    responsive drop down menu with svg image and Links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './App.css';
+import Header from './components/Header';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Videos from './pages/Videos';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Header />
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/blog" element={<Blog />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/portfolio" element={<Portfolio />} />
+    <Route path="/videos" element={<Videos />} />
+    </Routes>
+    </Router>
+    </>
   );
 }
 

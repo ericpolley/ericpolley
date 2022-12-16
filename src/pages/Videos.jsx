17a17fac4e1e3e3125e9cd2268068b1e7e6fb902
eric polley <ericpolley@live.com>
2022-12-16 11:01:28 -0800

    videos change description for more tutorials playlist

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -16,10 +16,11 @@ export default function Videos() {
       </div>
 
       <div className='flex flex-col w-full items-center bg-slate-200'>
-        <p className='text-2xl mb-4 lg:mt-4'>80 Second Tutorials</p>
+        <p className='text-2xl mb-4 lg:mt-4'>Tutorials</p>
         <iframe className="shadow-xl mb-4 w-[85vw] md:h-[350px] lg:w-[440px] lg:h-[250px]" width="560" height="235" src="https://www.youtube.com/embed/videoseries?list=PLce5_YZyrUw7cHSt2HV5esEjQJKOG2GHG" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         
         <p className='text-md lg:mb-12 mt-4 pt-2 pr-5 pl-5 pb-5'>
+        Some of my tutorials are very long, and some tutorials are 80 seconds. <br />
         I decided to create 80 second tutorials in order to challenge myself by relaying useful information quickly that may be indexed for others and my own future use. 
      </p>
       </div>

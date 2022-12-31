76f2512bb23dcaf90cc2f19345d723191808d9c7
eric polley <ericpolley@live.com>
2022-12-31 10:46:13 -0800

    img url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
       </p>
       <Link to="/portfolio" className='flex items-center justify-center'>
       <img className='rounded-full border-8 mb-4 border-slate-300 md:m-4 md:w-[30%] w-[70%]' 
-      src="https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/309513797_10228947023368621_5565939701859507103_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=ArItgucSD0QAX8IS9zL&_nc_ht=scontent-sea1-1.xx&oh=00_AfANrzPFFkl7UGoz01uIDoAMcYn8WUJbXZlZE2rTL82VCw&oe=63AF930B" alt="eric"></img>
+      src="https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/309513797_10228947023368621_5565939701859507103_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Xfdik5DhLeUAX-s2kfK&_nc_ht=scontent-sea1-1.xx&oh=00_AfD3d-o41b_P267Nlc9YMbF-3i3FWT5XiKxWkX40GUrgvQ&oe=63B581CB" alt="eric"></img>
      </Link>
      
      

b43f75d4809f2271403bd59040f694ac88ad96e2
eric polley <ericpolley@live.com>
2023-02-14 06:10:16 -0800

    New game added to portfolio

--- src/pages/GamePortfolio.jsx
+++ src/pages/GamePortfolio.jsx
@@ -7,6 +7,21 @@ export default function GamePortfolio() {
        <div className='w-full text-center'>
       <h1 className='text-2xl font-medium mb-2'>Games</h1>
       <div className='flex flex-row flex-wrap justify-center'>
+                {/* CARD */}
+      <div className='lg:w-[30%] w-full shadow-xl border-4 m-2 flex flex-col justify-center items-center text-black bg-slate-400 rounded-xl border-slate-500'>
+          <p className=' font-semibold text-lg m-2'>Frank the Jelly DEMO</p>
+          <a rel="noreferrer" className="flex justify-center "  target="_blank" href="https://erpolley.itch.io/diputs-demo">
+         <img src="https://img.itch.zone/aW1hZ2UvMTkyNTk1Mi8xMTMyMjM0OC5wbmc=/original/uSBukV.png" className=" shadow-lg w-[95%] h-[340px] border-4 rounded-md border-slate-500 object-cover" alt="frank" />
+         </a>
+          <p className='mb-2 mt-2 text-sm'>
+            
+            Platform side scroller PC Game. <br /> Created with UPBGE and Python. 
+            </p>
+          <div className='mb-2 mt-2'>
+          <a className='focus:text-blue-400 text-blue-700 hover:text-blue-300 m-8' rel="noreferrer"  target="_blank" href="https://erpolley.itch.io/diputs-demo">Download Now </a>
+          
+          </div></div> 
+        {/*Card end*/}
         {/* CARD */}
       <div className='lg:w-[30%] w-full shadow-xl border-4 m-2 flex flex-col justify-center items-center text-black bg-slate-400 rounded-xl border-slate-500'>
           <p className=' font-semibold text-lg m-2'>Frank the Jelly DEMO</p>
@@ -18,7 +33,7 @@ export default function GamePortfolio() {
             Platform side scroller PC Game. <br /> Created with UPBGE and Python. 
             </p>
           <div className='mb-2 mt-2'>
-          <a className='focus:text-blue-400 text-blue-700 hover:text-blue-300 m-8' rel="noreferrer"  target="_blank" href="https://drive.google.com/u/0/uc?id=1FFS0MwxkEapj2C80Z-pFspNWO8kmS_wL&amp;export=download&amp;confirm=t&amp;uuid=5817c4c6-384b-4edb-be90-def7727469e4&amp;at=ALgDtsy7ssr7dhUjoLrYdamaFod4:1675368119870">Download Now </a>
+          <a className='focus:text-blue-400 text-blue-700 hover:text-blue-300 m-8' rel="noreferrer"  target="_blank" href="https://erpolley.itch.io/frank-the-jelly-demo">Download Now </a>
           
           </div></div> 
         {/*Card end*/}
